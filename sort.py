060743760e4504cf0f20ed083e80d9a80c393778
Tyler <tyler.nychka@gmail.com>
2016-05-22 15:56:33 -0400

    Updated sorting method and improved functionality

--- sort.py
+++ sort.py
@@ -1,39 +1,76 @@
-#Organize files
 import os
-#current_directory = os.getcwd()									#get the current directory
-current_directory = "E:\\Users\\Tyler\\Downloads"
+import re
+
+sortFolders = {
+	"jpg" : "pictures",
+	"png" : "pictures",
+	"gif" : "pictures",
+
+	"exe" : "programs",
+	"cpp" : "programs",
+	"hpp" : "programs"
+}
+
+renamingRules = {	
+	"_by_.*" : "",
+	" by .*" : "",
+	"tumblr.*" : "unnamed",
+	"_" : " ",	
+}
+
+
+def rename(fileName):
+	for regex in renamingRules:
+		p = re.compile(regex)
+		fileName = re.sub(regex, renamingRules[regex], fileName)
+	return fileName
+
+
 
 def sortFile(directory, file, prefix, extention):
-	
-	print("Detected "+ extention)
-	
-	folder = extention.replace(".","")+"s"
-	
-	if not os.path.exists(os.path.join(directory,folder)):					#check to see if folder for extention exists
-		os.makedirs(os.path.join(directory,folder))
-		os.rename(os.path.join(directory,file), os.path.join(directory,folder,file))    #move file into new folder, duplicates will not occur since the folder is empty
-	elif not os.path.exists(os.path.join(directory,folder,file)):				#Prevent overwriting duplicate names
-		os.rename(os.path.join(directory,file), os.path.join(directory,folder,file))
+
+	if extention in sortFolders:
+		sortFolder = sortFolders[extention]
 	else:
-		n = 1
-		temp = '('+str(n)+')'
-		while os.path.exists(os.path.join(directory,folder,prefix+temp+extention)):
-			n = n+1										#renames a multiple of the original name
-			temp = '('+str(n)+')'
-		os.rename(os.path.join(directory,file), os.path.join(directory,folder,prefix+temp+extention))
+		sortFolder = "Unknown"
+
+	sortFolderExists = os.path.exists(os.path.join(directory,sortFolder))
+	fileLocation = os.path.join(directory,file)
+	fileDestination = fileLocation
+	prefix = rename(prefix)
+	fileNameExists = os.path.exists(os.path.join(directory,sortFolder,prefix+"."+extention))
+
+	if not sortFolderExists:							#check to see if folder for extention exists
+		os.makedirs(os.path.join(directory,sortFolder))
+		fileDestination = os.path.join(directory,sortFolder,prefix+"."+extention)
+	elif fileNameExists:								#Prevent overwriting duplicate names																			#If duplicate exists, rename to multiple	
+		n = 1	
+		temp = ' ('+str(n)+')'
+		while os.path.exists(os.path.join(directory,sortFolder,prefix+temp+"."+extention)):
+			n += 1									#renames a multiple of the original name
+			temp = ' ('+str(n)+')'
+		fileDestination = os.path.join(directory,sortFolder,prefix+temp+"."+extention)
+	else:
+		fileDestination = os.path.join(directory,sortFolder,prefix+"."+extention)
+	
+	if not fileDestination == fileLocation:
+		os.rename(fileLocation, fileDestination) #move file into new folder, duplicates will not occur since the folder is empty
 
 
 def organize(directory):
-	n=0
-	temp=""
 	list_of_file = os.listdir(directory)
 	for file in list_of_file:									#Search through file and place all major extentions in a directory 
-		#print("Now sorting " + file)
 		file_prefix = os.path.splitext(file)[0]
 		file_extention = os.path.splitext(file)[1]
-		if not file_extention == '':
-			sortFile(directory, file, file_prefix, file_extention)
-		
-#print (current_directory)
-#print (os.listdir(current_directory))
-organize(current_directory)
+		if (not file_extention == ''
+			and not file == "sort.py"):
+			sortFile(directory, file, file_prefix, file_extention.replace(".", ""))
+
+
+def main():
+	current_directory = os.getcwd()
+	print (current_directory)
+	organize(current_directory)
+
+
+main()
